2dbdad271a893a51f3ccc12a3e6dd1b976370f10
Srikanth <awstrialac@gmail.com>
2019-03-23 12:42:12 +0530

    removed spaces

--- HelloWorldServlet.java
+++ HelloWorldServlet.java
@@ -1,6 +1,5 @@
-	package com.boraji.tutorial.servlet;
+package com.boraji.tutorial.servlet;
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
